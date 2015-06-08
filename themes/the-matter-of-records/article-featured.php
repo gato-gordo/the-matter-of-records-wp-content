dbf86c4deed8b6731c004ffba89a85cbdf62310a
Ignacio Prado <ignacio@gatogordoweb.com>
2015-06-08 17:25:13 -0500

    --amend

--- themes/the-matter-of-records/article-featured.php
+++ themes/the-matter-of-records/article-featured.php
@@ -3,7 +3,7 @@
 		<?php the_post_thumbnail(); ?>
 	</a>
 	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-	<p class="post-meta">By <a href="author.html"><?php the_author(); ?></a>
+	<p class="post-meta">By <a href="<?php the_author_link(); ?>"><?php the_author(); ?></a>
 	</p>
 	<p><?php the_excerpt(); ?></p>
 </article>
