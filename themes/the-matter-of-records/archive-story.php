efb5409d0c150004d2ca4ec70c8e39f369a2d0ad
Ignacio Prado <ignacio@gatogordoweb.com>
2015-06-09 16:32:58 -0500

    Complete archive single for stories

--- themes/the-matter-of-records/archive-story.php
+++ themes/the-matter-of-records/archive-story.php
@@ -3,7 +3,7 @@
 <?php get_template_part('section', 'sub-mast'); ?>
 
 <section id="main">
-	<?php if(have_posts()): while(have_posts(): the_post(); ?>
+	<?php if(have_posts()): while(have_posts()): the_post(); ?>
 		<?php get_template_part('article', 'featured'); ?>
 	<?php endwhile; endif; ?>
 </section>
